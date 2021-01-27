29242afffe7b2c258d6a8cba7d12d4357f3051b2
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-01-27 15:34:42 +0000

    Set up form input and onChange handler

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import queryString from 'query-string'
 
 function Chat() {
+  const [value, setValue] = useState('')
+
+  // eslint-disable-next-line no-restricted-globals
+  const contact = queryString.parse(location.search).contact
+
+  const handleChange = (e) => {
+    setValue(() => e.target.value)
+  }
+
   return (
-    <h1>Chat</h1>
+    <div className='chat__container'>
+      <Link
+        to='/'
+      >
+        Back
+      </Link>
+      <h2>{contact}</h2>
+      <div className='chat__window'></div>
+      <form>
+        <input type='text' value={value} onChange={handleChange} />
+        <input type='submit' value='Submit' />
+      </form>
+      <p>{value}</p>
+    </div>
   )
 }
 
