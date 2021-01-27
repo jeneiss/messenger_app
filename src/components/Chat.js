1201766a7195a872afad4667832a37731dae08e7
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-01-27 15:41:41 +0000

    Add form and chat display func to Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,13 +3,35 @@ import { Link } from 'react-router-dom'
 import queryString from 'query-string'
 
 function Chat() {
+  const [chat, setChat] = useState({})
   const [value, setValue] = useState('')
 
   // eslint-disable-next-line no-restricted-globals
   const contact = queryString.parse(location.search).contact
 
   const handleChange = (e) => {
-    setValue(() => e.target.value)
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    if (chat[contact]) {
+      setChat(prevState => (
+        {
+          ...prevState,
+          [contact]: prevState[contact].concat(value)
+        }
+      ))
+    } else {
+      setChat(prevState => (
+        {
+          ...prevState,
+          [contact]: value
+        }
+      ))
+    }
+
+    setValue('')
+    e.preventDefault()
   }
 
   return (
@@ -20,12 +42,13 @@ function Chat() {
         Back
       </Link>
       <h2>{contact}</h2>
-      <div className='chat__window'></div>
-      <form>
+      <div className='chat__window'>
+        {chat[contact]}
+      </div>
+      <form onSubmit={handleSubmit}>
         <input type='text' value={value} onChange={handleChange} />
         <input type='submit' value='Submit' />
       </form>
-      <p>{value}</p>
     </div>
   )
 }
